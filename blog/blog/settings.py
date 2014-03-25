0dcb99b6af2eaaefc8128f94b9b664c78af43c22
saurabhzx <saurabhbhatiazx@gmail.com>
2014-03-26 00:03:19 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'NAME': 'Django_Blog',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'arink',
+        'PASSWORD': 'your password',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
